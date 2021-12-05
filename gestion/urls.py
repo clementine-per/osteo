7268c9de9fdc5a8c5ec1c2a7423e9bca55d25c5e
clementine-per <perreaut.clementine@gmail.com>
2021-12-05 16:57:38 +0100

    oubli

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,9 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
+from django.views.generic import DetailView
 
+from gestion.models.animal import Animal
 from gestion.views import home, animals
 
 urlpatterns = [
     path("", home, name="accueil"),
     path("animals/", animals.animal_list, name="animals"),
     path("animals/create", animals.CreateAnimal.as_view(), name="create_animal"),
+    path(
+        "animals/<int:pk>/",
+        login_required(
+            DetailView.as_view(
+                model=Animal,
+                template_name="gestion/animal/animal_detail.html",
+            )
+        ),
+        name="detail_animal",
+    ),
 ]
